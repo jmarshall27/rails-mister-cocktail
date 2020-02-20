ba16920a704d92babf58341c22031fe7019f96d6
Jake Marshall <jakemarshall27@icloud.com>
2020-02-20 15:18:04 +0000

    Finished cocktails controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# require 'json'
+# require 'open-uri'
+
+# url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+# ingredients_serialized = open(url).read
+# ingredients = JSON.parse(ingredients_serialized)
+
+# ingredients.each do |ingredient|
+#   Ingredient.create(name: ingredient)
+# end
+
+# puts "#{user['name']} - #{user['bio']}"
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
